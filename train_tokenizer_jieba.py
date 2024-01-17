c585f9ef6a21ef4c0b26b168ffd7653ad2797abc
Limour <93720049+Limour-dev@users.noreply.github.com>
2024-01-18 04:18:25 +0800

    Add files via upload

---
+++ train_tokenizer_jieba.py
@@ -0,0 +1,27 @@
+import jieba
+from tqdm import tqdm
+import os, pickle
+filePath = r'D:\datasets\h-corpus'
+filePath = [os.path.join(filePath, f) for f in os.listdir(filePath) if f.endswith('txt')]
+
+# wSet = {}
+with open('tmp_wSet.pkl', 'rb') as file:
+    wSet = pickle.load(file)
+
+print('start')
+
+for i in tqdm(range(2001, len(filePath)), desc="Converting"):
+    path = filePath[i]
+    with open(path, 'r', encoding='utf-8') as f:
+        tmp = f.read()
+    seg_list = jieba.cut(tmp)
+    for x in seg_list:
+        wSet[x] = wSet.get(x, 0) + 1
+
+    if i%1000 == 0 :
+        with open('tmp_wSet.pkl', 'wb') as file:
+            pickle.dump(wSet, file)
+        print(i)
+
+
+

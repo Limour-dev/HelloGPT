8d2a7bed697128f5b8fb8cf191165883cbd306b8
Limour <93720049+Limour-dev@users.noreply.github.com>
2024-01-20 19:30:22 +0800

    Add files via upload

--- h_corpus.py
+++ h_corpus.py
@@ -1,10 +1,22 @@
 import os
+import gzip
+
+def get_all_files_in_directory(directory):
+    all_files = []
+    for root, dirs, files in os.walk(directory):
+        root = root[len(directory):]
+        if root.startswith('\\') or root.startswith('/'):
+            root = root[1:]
+        for file in files:
+            file_path = os.path.join(root, file)
+            all_files.append(file_path)
+    return all_files
 
 class Fileset(list):
     def __init__(self, path, ext='', _read=None):
         if isinstance(path, str):
             self.root = path
-            self.extend(f for f in os.listdir(self.root) if f.endswith(ext))
+            self.extend(f for f in get_all_files_in_directory(self.root) if f.endswith(ext))
             self._read = _read
 
     def __getitem__(self, index):
@@ -20,10 +32,6 @@ class Fileset(list):
             fileset.extend(super().__getitem__(index))
             return fileset
 
-    def getFileName(self, index):
-        fname, ext = os.path.splitext(super().__getitem__(index))
-        return fname
-
 
 from tokenizer import tokenizer
 token_eos = 2
@@ -31,7 +39,7 @@ token_eos = 2
 
 def readOne(filePath):
     retn = []
-    with open(file=filePath, encoding='utf-8') as f:
+    with gzip.open(filePath, 'rt', encoding='utf-8') if filePath.endswith('.gz') else open(filePath, encoding='utf-8') as f:
         for line in f:
             retn += tokenizer.encode(line).ids
     retn.append(token_eos)
@@ -68,15 +76,19 @@ class Hcorpus():
         return f"Hcorpus(r'{self.fileset.root}', fileset_idx={self.fileset_idx-1}, fileset_sub_idx={self.fileset_sub_idx})"
 
 if __name__ == '__main__':
+    # filePath = r'D:\datasets\h-corpus'
+    # tmp = Hcorpus(filePath)
+    # for i in range(10):
+    #     tmp2 = tmp()
+    #     tmp3 = tokenizer.decode(tmp2)
+    #     print(tmp2, '\n', tmp3, '\n', len(tmp3)/len(tmp2))
+    # print(tmp)
+    # print(tokenizer.decode(tmp()))
+    # tmp = Hcorpus(r'D:\datasets\h-corpus', fileset_idx=1, fileset_sub_idx=132)
+    # print(tokenizer.decode(tmp()))
+    # print(tmp)
     filePath = r'D:\datasets\h-corpus'
-    tmp = Hcorpus(filePath)
-    for i in range(10):
-        tmp2 = tmp()
-        tmp3 = tokenizer.decode(tmp2)
-        print(tmp2, '\n', tmp3, '\n', len(tmp3)/len(tmp2))
-    print(tmp)
+    tmp = Hcorpus(filePath, ext='.txt.gz')  # pigz *
+    # 2..20 | ForEach-Object {cd "D:\datasets\h-corpus\h-corpus-s$($_.ToString('D2'))"; pigz *.txt}
     print(tokenizer.decode(tmp()))
-    tmp = Hcorpus(r'D:\datasets\h-corpus', fileset_idx=1, fileset_sub_idx=132)
-    print(tokenizer.decode(tmp()))
-    print(tmp)
 
